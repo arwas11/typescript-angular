dbb821ff55b413f495d7fed715cf3820df903299
Arwa Sharif <arwas@google.com>
2023-11-03 16:13:44 -0500

    initial commit

---
+++ dist/model.js
@@ -0,0 +1,3 @@
+var displayName = '1234';
+var displayName1 = '1234';
+console.log(displayName1);
---
+++ model.ts
@@ -0,0 +1,6 @@
+let displayName:string = '1234';
+let displayName1:string = '1234';
+
+
+console.log(displayName1)
+
